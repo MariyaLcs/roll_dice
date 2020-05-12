46b4d4237aff6450762993c1e90a873f85ae6519
Mariya <mariya.podosinova@gmail.com>
2020-05-12 16:59:49 +0100

    roll() function

--- src/RollDice.js
+++ src/RollDice.js
@@ -2,22 +2,30 @@ import React, { Component } from "react";
 import Die from "./Die";
 
 class RollDice extends Component {
+  static defaultProps = {
+    sides: ["one", "two", "three", "four", "five", "six"],
+  };
   constructor(props) {
     super(props);
-    this.state = { num: "one" };
+    this.state = { die1: "one", die2: "six" };
     this.roll = this.roll.bind(this);
   }
 
   roll() {
-    let rand = Math.floor(Math.random() * 6) + 1;
-    this.setState({ num: rand.toString() });
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    this.setState({ die1: newDie1, die2: newDie2 });
   }
+
   render() {
     return (
       <div>
-        <h1>{this.state.num}</h1>
-        <Die count={this.state.num} />
-        <Die count={this.state.num} />
+        <Die face={this.state.die1} />
+        <Die face={this.state.die2} />
         <button onClick={this.roll}>Roll Dice</button>
       </div>
     );
