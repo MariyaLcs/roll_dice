cef6a20b86201070bc0abd3fd9766ad8badf6768
Mariya <mariya.podosinova@gmail.com>
2020-05-12 15:05:13 +0100

    my as far i can go

---
+++ src/RollDice.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Die from "./Die";
+
+class RollDice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: "one" };
+    this.roll = this.roll.bind(this);
+  }
+
+  roll() {
+    let rand = Math.floor(Math.random() * 6) + 1;
+    this.setState({ num: rand.toString() });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.num}</h1>
+        <Die count={this.state.rand} />
+        <Die count={this.state.rand} />
+        <button onClick={this.roll}>Roll Dice</button>
+      </div>
+    );
+  }
+}
+
+export default RollDice;
