8b964f8cc10a8943229a830c274c6c59bebd6226
Mariya <mariya.podosinova@gmail.com>
2020-05-12 17:25:10 +0100

    Updates

---
+++ precache-manifest.54e74c7489e59ab3ba4ef8a07205c0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998a22937507460f70f266afe86222dd",
+    "url": "/roll_dice/index.html"
+  },
+  {
+    "revision": "f81cfdbab425f6d29bba",
+    "url": "/roll_dice/static/css/main.07dc76a7.chunk.css"
+  },
+  {
+    "revision": "4ea1fc6d7a42487e1131",
+    "url": "/roll_dice/static/js/2.00e8ba78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roll_dice/static/js/2.00e8ba78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81cfdbab425f6d29bba",
+    "url": "/roll_dice/static/js/main.276b28a4.chunk.js"
+  },
+  {
+    "revision": "8f9b477bcdf806da365e",
+    "url": "/roll_dice/static/js/runtime-main.982d2a3d.js"
+  }
+]);
